37561e8e2a0d75bbe241946bbd0905c0b10cf2aa
fohalabi <ayomideabimbola79@gmail.com>
2026-02-20 02:28:35 +0100

    Add authentication types and update dependencies in package.json

---
+++ server/src/types/auth.types.ts
@@ -0,0 +1,24 @@
+export interface RegisterInput {
+    email: string;
+    password: string;
+    name: string;
+}
+
+export interface LoginInput {
+    email: string;
+    password: string;
+}
+
+export interface JwtPayload {
+    userId: string;
+    email: string;
+}
+
+export interface AuthResponse {
+    user: {
+        id: string;
+        email: string;
+        name: string;
+    };
+    token: string;
+}
